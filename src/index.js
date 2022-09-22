01bdb73b408486aba19b5b43aecaf627151645ce
Newton <altontonnalumasa@gmail.com>
2022-09-22 20:36:02 +0300

    add some space

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
-// component file
+
 import TodoContainer from './components/TodoContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
