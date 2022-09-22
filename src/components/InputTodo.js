8bda5fdb3ad7ef5c8352df1cc7439554859344d0
Newton <altontonnalumasa@gmail.com>
2022-09-22 18:50:00 +0300

    add react to-do app

---
+++ src/components/InputTodo.js
@@ -0,0 +1,48 @@
+import React from 'react';
+/*eslint-disable */
+
+class InputTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+    };
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { titleText } = this.state;
+    const { addTodo } = this.props;
+    if (titleText.title.trim()) {
+      addTodo.addTodoProps(titleText.title);
+      this.setState({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add Todo..."
+          name="title"
+          value={this.state.title}
+          onChange={this.onChange}
+        />
+        <button type="submit" className="input-submit">Submit</button>
+      </form>
+    );
+  }
+}
+export default InputTodo;
